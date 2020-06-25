fd280940d3705c8ef788303bafe62fb585a15dbd
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2020-06-25 14:21:39 +0530

    class methods added

--- Employee.py
+++ Employee.py
@@ -17,23 +17,34 @@ class Employee:
     def apply_raise(self):
         self.pay*=self.raise_amount
 
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amount=amount
+    
+    @classmethod
+    def init_from_String(cls,empString):    #class method as an alternate constructor
+        first,last,pay=empString.split("-")
+        return cls(first,last,float(pay))
+
 
 #initializing the class instances
 emp1=Employee("Ramesh","Nanavati",75000)
 emp2=Employee("Raghav","Shukla",70000)
+
+#using the alternate constructors made by a class method
+emp3=Employee.init_from_String("John-Doe-60000")
+emp4=Employee.init_from_String("Jane-Doe-60000")
 
 #2 ways of calling class methods
 print(emp1.fullname())
 print(Employee.fullname(emp2))
 
 print(emp1.email)   #outputting the attribute of the instance
-print(emp2.email)
+print(emp3.email)
 
+#modify raise using class method
+Employee.set_raise_amt(1.05)
 emp2.apply_raise()
+emp4.apply_raise()
 print(emp2.pay)
-
-print(emp1.__dict__)
-emp1.raise_amount=1.05
-print(emp1.raise_amount)
-print(Employee.raise_amount)
-print(emp1.__dict__)
+print(emp4.pay)
